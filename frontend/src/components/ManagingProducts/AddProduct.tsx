5ae0d55a41cc204b2e337d7c7da29e48d0fd2407
Kamil <k.mirgasimov@innopolis.university>
2024-07-22 00:04:32 +0300

    bugfix

--- frontend/src/components/ManagingProducts/AddProduct.tsx
+++ frontend/src/components/ManagingProducts/AddProduct.tsx
@@ -50,9 +50,7 @@ export default function AddProduct() {
     }).catch((error) => alert(error));
 
     const imageRef = storageRef(storage, `images/${guid}`);
-    uploadBytes(imageRef, main_image).then(() => {
-      alert('image uploaded');
-    });
+    uploadBytes(imageRef, main_image);
   }
 
   return (
